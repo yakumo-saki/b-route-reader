6058eec9421b887ed339401e1350742d3f4298c7
Yakumo Saki <yakumo@ziomatrix.org>
2018-12-19 02:54:25 +0900

    値の解釈を追加

--- const-epc.js
+++ const-epc.js
@@ -2,10 +2,10 @@
 
 // クラス変数
 let EPC = {
-	DELTA_DENRYOKU: "E0",
-	NOW_DENRYOKU: "E7",
-	NOW_DENRYUU: "E8",
-	DELTA_HISTORY: "E2",
+	DELTA_DENRYOKU: "e0",
+	NOW_DENRYOKU: "e7",
+	NOW_DENRYUU: "e8",
+	DELTA_HISTORY: "e2",
 }
 
 module.exports = EPC;
--- echonet.js
+++ echonet.js
@@ -21,7 +21,7 @@ log4js.configure(
 			},
 			power: {
 				type: "file",
-				filename: "log/powerdata.log",
+				filename: "/var/log/environment/powerdata.log",
 				layout: {
 					type: "pattern",
 					pattern: "%m"
@@ -93,26 +93,22 @@ var elsocket = EL.initialize( objList, function( rinfo, els, err ) {
 
 	var logger = global.logger;
 
-	if (!global.waifForAnswer) {
-		return;
-	}
-
 	// GET(0x62) の応答 (0x72)
 	if (els.DEOJ === '05ff01' && els.ESV === EL.GET_RES) {
 		// logger.debug(els.ESV);
-		logger.debug("=---------=");
-		logger.debug(rinfo);
-		logger.debug("=---------=");
-		logger.debug(els);
-		logger.debug("=---------=");
-		logger.debug(els.DETAILs);
-		logger.debug("e0=" + els.DETAILs["e0"] + " " + parseInt(els.DETAILs["e0"], 16));
-		logger.debug("e7=" + els.DETAILs["e7"] + " " + parseInt(els.DETAILs["e7"], 16));
-		logger.debug("e8=" + els.DETAILs["e8"] + " " + parseInt(els.DETAILs["e8"], 16));
+		// logger.debug("=---------=");
+		// logger.debug(rinfo);
+		// logger.debug("=---------=");
+		// logger.debug(els);
+		// logger.debug("=---------=");
+		// logger.debug(els.DETAILs);
+		// logger.debug("e0=" + els.DETAILs["e0"] + " " + parseInt(els.DETAILs["e0"], 16));
+		// logger.debug("e7=" + els.DETAILs["e7"] + " " + parseInt(els.DETAILs["e7"], 16));
+		// logger.debug("e8=" + els.DETAILs["e8"] + " " + parseInt(els.DETAILs["e8"], 16));
 
 		for (var i = 0; i < global.get_properties.length; i++) {
 			var prop = global.get_properties[i].toLowerCase();
-			logger.debug(prop + " " + els.DETAILs[prop]);
+			// logger.debug(prop + " " + els.DETAILs[prop]);
 			if (els.DETAILs[prop] != undefined) {
 				global.result[prop] = els.DETAILs[prop];
 			}
@@ -120,27 +116,76 @@ var elsocket = EL.initialize( objList, function( rinfo, els, err ) {
 	}
 });
 
-// NetworkのELをすべてsearchしてみよう．
-// search ECHONET nodes in local network
-// EL.search();
 
-//EL.sendOPC1 = function( ip, seoj, deoj, esv, epc, edt)
+// 問い合わせを送信。ただし、連続で送ると相手の負荷が怖いので1秒ずつ間を開ける
+global.result["datetime"] = new Date().toISOString();
 
-if (!global.waifForAnswer) {
-	global.waifForAnswer = true;
+for (var i = 0; i < get_properties.length; i++) {
+	setTimeout(function(prop) {
+		logger.debug("send req " + prop);
 
-	// 問い合わせを送信。ただし、連続で送ると相手の負荷が怖いので1秒ずつ間を開ける
-	global.result["datetime"] = new Date().toISOString();
-	for (var i = 0; i < get_properties.length; i++) {
-		setTimeout(function(prop) {
-			logger.debug("send req " + prop);
-			EL.sendOPC1('10.1.0.100', '05ff01', '028801', EL.GET, prop, "");
-		},(i * 1000), global.get_properties[i]);
-	}
+		//EL.sendOPC1 = function( ip, seoj, deoj, esv, epc, edt)
+		EL.sendOPC1('10.1.0.100', '05ff01', '028801', EL.GET, prop, "");
+	},(i * 1000), global.get_properties[i]);
 }
 
+
+// 終了判定（適当過ぎるので後で直す）
 setTimeout(function(sock) {
+	// 値の解釈
+
+	// e2（積算履歴）は別途解釈が必要
+  var e2 = parse_e2(global.result[EPC.DELTA_HISTORY]);
+  var e0 = parse_e0(global.result[EPC.DELTA_DENRYOKU]);
+  var e7 = parse_e7(global.result[EPC.NOW_DENRYOKU]);
+  var e8 = parse_e8(global.result[EPC.NOW_DENRYUU]);
+
+  global.result = Object.assign(global.result, e2, e0, e7, e8);
+
 	global.power_logger.info(JSON.stringify(global.result));
 	logger.debug("Exiting.");
 	sock.close();
 } , 15000, elsocket);
+
+function parse_e2(e2_value) {
+
+	const e2_keys = ["0000","0030","0100","0130","0200","0230","0300","0330","0400","0430"
+                  , "0500","0530","0600","0630","0700","0730","0800","0830","0900","0930"
+                  , "1000","1030","1100","1130","1200","1230","1300","1330","1400","1430"
+                  , "1500","1530","1600","1630","1700","1730","1800","1830","1900","1930"
+                  , "2000","2030","2100","2130","2200","2230","2300","2330"]
+
+  for (var i = 0; i < 48; i++) {
+    var key = "history_kwh_" + e2_keys[i];
+    if (e2_value != undefined) {
+      var hex = e2_value.substr(4 + (i * 8), 8);
+      global.result[key] = hex_to_decimal(hex);
+    } else {
+      global.result[key] = 0;
+    }
+  }
+}
+
+/// e0 積算電力量計測値（正方向）
+function parse_e0(value) {
+  return {delta_kwh: hex_to_decimal(value)}
+}
+
+/// e7 瞬時電力量計測値（正方向）
+function parse_e7(value) {
+  return {now_w: hex_to_decimal(value)}
+}
+
+/// e8 瞬時電流計測値（正方向）
+function parse_e8(value) {
+  var r = hex_to_decimal(value.substr(0, 4) );
+  var t = hex_to_decimal(value.substr(4, 4) );
+  var total = r + t;
+
+  return {now_R_amp: r, now_T_amp: t, now_total_amp: total}
+}
+
+
+function hex_to_decimal(value) {
+  return parseInt(value, 16);
+}
